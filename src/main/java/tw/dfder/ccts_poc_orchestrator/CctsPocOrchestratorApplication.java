7b89a76359902d945d32a655936c17e09a5d2030
dfder <a2470abc@gmail.com>
2022-02-18 16:48:34 +0800

    Initial commit

---
+++ src/main/java/tw/dfder/ccts_poc_orchestrator/CctsPocOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package tw.dfder.ccts_poc_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctsPocOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CctsPocOrchestratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/dfder/ccts_poc_orchestrator/CctsPocOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.dfder.ccts_poc_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctsPocOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
