160d9176b58c1f13dc6b8db9b1dcbe0276269399
dfder <a2470abc@gmail.com>
2022-02-18 23:55:58 +0800

    feat: init orchestrator.

--- src/main/java/tw/dfder/ccts_poc_orchestrator/controller/Orchestrator.java
+++ src/main/java/tw/dfder/ccts_poc_orchestrator/controller/Orchestrator.java
@@ -1,10 +1,19 @@
 package tw.dfder.ccts_poc_orchestrator.controller;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@EnableRabbit
 public class Orchestrator {
+    private final RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    public Orchestrator(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
 
 
